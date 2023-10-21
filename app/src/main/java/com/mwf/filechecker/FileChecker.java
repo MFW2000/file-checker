e34b1fbf8dcaf61a3e63fa84a9db3dc428e5c23f
MFW2000 <marijnwelmerink@gmail.com>
2023-10-22 01:21:01 +0200

    Add file check base

---
+++ app/src/main/java/com/mwf/filechecker/FileChecker.java
@@ -0,0 +1,16 @@
+package com.mwf.filechecker;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileChecker {
+    public static List<String> listFilesWithGivenSymbol(String directoryPath, char symbol) {
+        List<String> filesWithGivenSymbol = new ArrayList<>();
+        File directory = new File(directoryPath);
+
+        // TODO: Implement method
+
+        return null;
+    }
+}
