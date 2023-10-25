5599466bcd9c17533fb00481be392d29e18ef14f
MFW2000 <marijnwelmerink@gmail.com>
2023-10-25 16:25:02 +0200

    Add Javadoc for exception classes

--- app/src/main/java/com/mwf/filechecker/exception/FileCheckerException.java
+++ app/src/main/java/com/mwf/filechecker/exception/FileCheckerException.java
@@ -1,6 +1,13 @@
 package com.mwf.filechecker.exception;
 
+/**
+ * Thrown to indicate that something went wrong with a certain {@code FileChecker} method.
+ */
 public class FileCheckerException extends Exception {
+    /**
+     * Constructs an {@code FileCheckerException} with the specified detail message.
+     * @param message the detail message
+     */
     public FileCheckerException(String message) {
         super(message);
     }
--- app/src/main/java/com/mwf/filechecker/exception/InvalidInputException.java
+++ app/src/main/java/com/mwf/filechecker/exception/InvalidInputException.java
@@ -1,6 +1,13 @@
 package com.mwf.filechecker.exception;
 
+/**
+ * Thrown to indicate that invalid input was provided.
+ */
 public class InvalidInputException extends Exception {
+    /**
+     * Constructs an {@code InvalidInputException} with the specified detail message.
+     * @param message the detail message
+     */
     public InvalidInputException(String message) {
         super(message);
     }
